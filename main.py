522dfa6bf033c39fd947ba707c8105ea17b542d8
omersabri <69110814+omersabri@users.noreply.github.com>
2026-06-16 12:54:59 +0000

    Incorrect total calculation

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def calculate_total(price, quantity):
-    return price * quantity
+    return price + quantity
+
 def display_total(total):
     print(f"The total is £{total}")
+
 def generate_summary(total):
-    return f"Summary: the final total is £{total}" 
-price = 5 
-quantity = 3 
+    return f"Summary: the final total is £{total}"
+
+price = 5
+quantity = 3
 total = calculate_total(price, quantity)
+
 display_total(total)
 print(generate_summary(total))
